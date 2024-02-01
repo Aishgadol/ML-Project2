0c7dcc01e888b4eefe12dbafa2b656af46d4b6ed
Idan Morad <81802815+Aishgadol@users.noreply.github.com>
2024-02-01 14:31:39 +0200

    more preprocessing

--- Perceptron.py
+++ Perceptron.py
@@ -6,8 +6,10 @@ from sklearn.preprocessing import MinMaxScaler
 
 column_names = ['Feature1', 'Feature2', 'Label']
 df=pd.read_csv('https://sharon.srworkspace.com/ml/datasets/hw2/exams.csv',header=None,names=column_names)
-#print(df)
-#seperate data to features and labels
+#keep the original data somewhere, we'll scale it later for easier use
+orig_data=df.drop('Label',axis=1).to_numpy()
+orig_labels=df['Label'].to_numpy()
+# #seperate data to features and labels
 data=df.drop('Label',axis=1).to_numpy()
 labels=df['Label'].to_numpy()
 #set all labels 0 to -1 to work with perceptron algorithm
@@ -27,3 +29,12 @@ def plotPoints(data):
     # Show the plot
     plt.show()
 #plotPoints(data)
+#create scaler
+mm_scaler=MinMaxScaler()
+#fit the data and transform it according to scaler
+orig_data=mm_scaler.fit_transform(orig_data)
+data=mm_scaler.fit_transform(data)
+#add 1's in dimension 0 to take bias into consideration
+data = np.insert(data, 0, 1, axis=1)
+#multiply every data that has label -1 with -1 so that for all x in dataset, g_i(x)>0
+data[labels==-1]*=(-1)
