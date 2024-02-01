751bb8422973bfbc0f1096aabc2c8e1f31d1e405
Idan Morad <81802815+Aishgadol@users.noreply.github.com>
2024-02-01 11:43:33 +0200

    fixing repo

--- Perceptron.py
+++ Perceptron.py
@@ -1,2 +1,13 @@
 import numpy as np
-print(f'hell')
+import pandas as pd
+import matplotlib.pyplot as plt
+from IPython.display import display, clear_output
+
+column_names = ['Feature1', 'Feature2', 'Label']
+df=pd.read_csv('https://sharon.srworkspace.com/ml/datasets/hw2/exams.csv',header=None,names=column_names)
+#print(df)
+#seperate data to features and labels
+data=df.drop('Label',axis=1).to_numpy()
+labels=df['Label'].to_numpy()
+#set all labels 0 to -1 to work with perceptron algorithm
+labels[labels==0]=-1
