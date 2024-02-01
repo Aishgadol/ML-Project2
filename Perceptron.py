6470644113371332e5ccb1451524859bb7af3595
Idan Morad <81802815+Aishgadol@users.noreply.github.com>
2024-02-01 19:30:18 +0200

    more preprocessing

--- Perceptron.py
+++ Perceptron.py
@@ -39,3 +39,76 @@ data=mm_scaler.fit_transform(data)
 data = np.insert(data, 0, 1, axis=1)
 #multiply every data that has label -1 with -1 so that for all x in dataset, g_i(x)>0
 data[labels==-1]*=(-1)
+
+def perceptron(data, labels, lr = 1.0):
+  # Implement here
+  np.random.seed(42)
+  #w=np.random.rand(len(data[0]))
+  w=np.ones(len(data[0]))
+  batch_size=10
+  num_iterations=1000
+  #approach 1, works, sorta
+  for _ in range(num_iterations):
+      for sample_index,sample in enumerate(data):
+        if(w.T @ sample <0):
+            w = w + lr * sample
+  '''
+  this approach tried to first sum all missclassifications per iteration, didnt work
+  for _ in range(num_iterations):
+      missclass_samples=np.zeros(len(data[0]))
+      for sample in data:
+          if(w.T @ sample <0):
+              missclass_samples+=sample
+      w = w + lr *missclass_samples
+      
+  mini batch approach, got it closer to real but needs more fine tuning
+  for itr in range(num_iterations):
+    batch_indices=np.random.choice(len(data),size=batch_size, replace=False)
+    batch=data[batch_indices]
+    batch_labels=labels[batch_indices]
+    missclass=[]
+    for sample in batch:
+        if(w.T @ sample<0):
+            w=w+lr*sample
+    '''
+  return w
+
+def plotAll(data,labels,w,bias,title):
+    plt.scatter(data[labels == -1, 0], data[labels == -1, 1], label='Label -1', alpha=0.8)
+    plt.scatter(data[labels == 1, 0], data[labels == 1, 1], label='Label 1', alpha=0.7)
+    a, b, c = w[0], w[1], bias
+    m = -a / b
+    b = -c / b
+
+	# Generate some x values for the plot
+    x = np.arange(np.min(data[:,0]), np.max(data[:,0]), 0.1)
+
+	# Compute the corresponding y values using the equation of the line
+    y= m * x +b
+	# Plot the line
+    plt.plot(x, y)
+
+    # Set labels and title
+    plt.xlabel('Feature 1')
+    plt.ylabel('Feature 2')
+    plt.title(title)
+
+    # Show legend
+    plt.legend()
+
+    # Show the plot
+    plt.show()
+
+weights=perceptron(data,labels)
+print(weights)
+#plotAll(orig_data,labels,weights[1:],weights[0])
+
+
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import accuracy_score
+sk_p=Perceptron(tol=1e-3, random_state=42)
+sk_p.fit(orig_data,orig_labels)
+plotAll(orig_data,labels,sk_p.coef_[0],sk_p.intercept_[0],"sklearn perceptron line")
+plotAll(orig_data,labels,weights[1:],weights[0],"my perceptron line ")
+print(f'sklearn perceptron weights: {sk_p.coef_[0]}\nMy weights: {weights[1:]}'
+      f'\nsk perc bias:{sk_p.intercept_[0]}\n my bias:{weights[0]}')
